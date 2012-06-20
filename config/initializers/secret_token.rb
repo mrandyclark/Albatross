cf00a9f5c206ab6b474de8d27454729b2a86ea71
Andrew Clark <mrandyclark@gmail.com>
2012-06-20 11:07:55 -0600

    firsties

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albatross::Application.config.secret_token = '8dc732a4fd36020919c3bb64fd3a67e384c4ad1d0300533fa7eeb06615eb09d7032643e4cab189f0fecc703f394818c9463658c7292efa944b5f3caf0f0740bc'
