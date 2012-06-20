c8928a9336d3db93ffd6ea64b8c24def4da438a6
Andrew Clark <mrandyclark@gmail.com>
2012-06-20 14:33:53 -0600

    add partial for player listing, creates admin page, default route

--- config/routes.rb
+++ config/routes.rb
@@ -53,9 +53,12 @@ Albatross::Application.routes.draw do
   match 'leaderboard', :to => 'leaderboard#index'
   match 'profile', :to => 'profile#profile'
   match 'scorecard', :to => 'scorecard#index'
+  match 'admin', :to => 'admin#dashboard'
   
   # See how all your routes lay out with "rake routes"
 
+  match '/:controller(/:action(/:id))'
+  
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
